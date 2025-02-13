34c55fa6b9af9f0d1b64f7fbe8c80a54c12f2ef8
Dylan Anderson <magiical25@gmail.com>
2025-02-13 13:31:44 -0600

    Improve mouse movement system to work correctly in combination with key button movement system.

--- src/player.rs
+++ src/player.rs
@@ -1,12 +1,19 @@
 use bevy::prelude::*;
 use bevy::sprite::Anchor;
 use bevy::window::PrimaryWindow;
+use bevy::core::Name;
 
 const GROUND_LEVEL: f32 = -100.0;
 const PLAYER_X: f32 = -300.0;
 
 #[derive(Component)]
 pub struct Player;
+
+#[derive(Component)]
+pub struct Movement {
+    free_move: bool,
+    destination: Vec2
+}
 
 #[derive(Component)]
 pub struct Ground;
@@ -23,6 +30,7 @@ pub fn setup(mut commands: Commands) {
 
     // Player
     commands.spawn((
+        Name::new("Player".to_string()),
         Player,
         Sprite {
             color: Color::srgb(1.0, 0.75, 0.0),
@@ -31,7 +39,11 @@ pub fn setup(mut commands: Commands) {
             ..default()
         },
         Transform::from_xyz(PLAYER_X, GROUND_LEVEL, 0.0),
-        Velocity(Vec2::ZERO),
+        Movement {
+            free_move: true,
+            destination: Vec2::new(PLAYER_X, GROUND_LEVEL),
+        },
+        Velocity(Vec2::ZERO)
     ));
 
     // One tree?
@@ -49,28 +61,32 @@ pub fn setup(mut commands: Commands) {
 
 pub fn handle_key_input(
     keys: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut Velocity, With<Player>>,
+    mut query: Query<(&mut Velocity, &mut Movement), With<Player>>,
 )
 {
-    if let Ok(mut velocity) = query.get_single_mut() {
+    for (mut velocity, mut movement) in query.iter_mut() {
         let mut x_movement_pressed = false;
         let mut y_movement_pressed = false;
 
         if keys.pressed(KeyCode::KeyD) {
             velocity.0.x = 250.0;
             x_movement_pressed = true;
+            movement.free_move = true;
         }
         if keys.pressed(KeyCode::KeyA) {
             velocity.0.x = -250.0;
             x_movement_pressed = true;
+            movement.free_move = true;
         }
         if keys.pressed(KeyCode::KeyS) {
             velocity.0.y = -250.0;
             y_movement_pressed = true;
+            movement.free_move = true;
         }
         if keys.pressed(KeyCode::KeyW) {
             velocity.0.y = 250.0;
             y_movement_pressed = true;
+            movement.free_move = true;
         }
         if !x_movement_pressed {
             velocity.0.x = 0.0;
@@ -85,10 +101,10 @@ pub fn handle_mouse_input(
     buttons: Res<ButtonInput<MouseButton>>,
     window: Query<&Window, With<PrimaryWindow>>,
     cameras: Query<(&Camera, &GlobalTransform), With<Camera2d>>,
-    mut query: Query<(&mut Velocity, &Transform), With<Player>>,
+    mut query: Query<&mut Movement, With<Player>>,
 )
 {
-    for (mut velocity, transform) in query.iter_mut() {
+    for (mut movement) in query.iter_mut() {
         if buttons.pressed(MouseButton::Left) {
             if let Some(mouse_position) = window.single().cursor_position() {
                 // Get the camera information
@@ -98,16 +114,9 @@ pub fn handle_mouse_input(
                         camera_transform,
                         mouse_position
                     ) {
-                        println!("World mouse position: {:?}", world_position);
-
-                        let x_velocity_component = (world_position.x - transform.translation.x).min(225.0) + 25.0;
-                        let y_velocity_component = (world_position.y - transform.translation.y).min(225.0) + 25.0;
-
-                        println!("X velocity: {:?}", x_velocity_component);
-                        println!("Y velocity: {:?}", y_velocity_component);
-
-                        velocity.0.x = x_velocity_component;
-                        velocity.0.y = y_velocity_component;
+                        // A world position has been captured, free moving is disabled now while we're targeting a destination.
+                        movement.destination = world_position;
+                        movement.free_move = false;
                     }
                 }
             }
@@ -117,12 +126,34 @@ pub fn handle_mouse_input(
 
 pub fn update_movement(
     time: Res<Time>,
-    mut query: Query<(&mut Transform, &mut Velocity), With<Player>>
+    mut query: Query<(&mut Transform, &Velocity, &mut Movement), With<Player>>,
 )
 {
-    for (mut transform, mut velocity) in query.iter_mut() {
-        let new_translation_x = transform.translation.x + velocity.0.x * time.delta_secs();
-        let new_translation_y = transform.translation.y + velocity.0.y * time.delta_secs();
+    for (mut transform, velocity, mut movement) in query.iter_mut() {
+        let mut new_translation_x = transform.translation.x;
+        let mut new_translation_y = transform.translation.y;
+
+        if(movement.free_move) {
+            new_translation_x = transform.translation.x + velocity.0.x * time.delta_secs();
+            new_translation_y = transform.translation.y + velocity.0.y * time.delta_secs();
+        }
+        else {
+            // Determine how much x translation to apply.
+            if(movement.destination.x > transform.translation.x) {
+                new_translation_x = transform.translation.x + 250.0 * time.delta_secs();
+            }
+            else {
+                new_translation_x = transform.translation.x + -250.0 * time.delta_secs();
+            }
+
+            // Determine how much y translation to apply
+            if(movement.destination.y > transform.translation.y) {
+                new_translation_y = transform.translation.y + 250.0 * time.delta_secs();
+            }
+            else {
+                new_translation_y = transform.translation.y + -250.0 * time.delta_secs();
+            }
+        }
 
         transform.translation.x = transform.translation.x.lerp(new_translation_x, 0.95);
         transform.translation.y = transform.translation.y.lerp(new_translation_y, 0.95);
